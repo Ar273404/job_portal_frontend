49fc54beff5b6acf0e92bd8fc29b24da286a85ae
Ar273404 <arun273404@gmail.com>
2024-07-16 15:41:56 +0530

    some

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/": {
+        target: "http://localhost:4000", // Replace with your backend server address
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
   build: {
     outDir: "dist",
   },
